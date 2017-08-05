27bc22b747c37ae638a0c1b02180be8e3c09c75b
ktalipski <k.talipski@gmail.com>
2017-08-06 00:21:01 +0200

    warsztaty sit on chair JS

---
+++ jquery_functions.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+$("#black_math").mouseover(function(){
+    $("#rightPhoto").toggle();
+});
+
+$("#seven_nation_army").mouseover(function(){
+    $("#lefttPhoto").toggle();
+});
+
+
+
+
+
+
+
+
+
+});
